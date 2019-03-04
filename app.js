1fa3af059c4e01628be94b95b08c222a34e33fc4
Denis Araki <scaramouchedpa@pc.speedport.ip>
2019-03-04 15:09:28 +0100

    Static pages splitted, Airtable API for timeline added

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-//app.js
-const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write(req.url);
-  res.end();
+//using the body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/scripts', express.static(__dirname + '/public/scripts'));
+
+app.get('/', (req, res) => {
+  res.sendFile("/public/home/home.html", { root: __dirname});
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/timeline', (req, res) => {
+  res.sendFile("/public/timeline/timeline.html", { root: __dirname});
 });
+
+app.get('/aboutus', (req, res) => {
+  res.sendFile("/public/aboutus/aboutus.html", { root: __dirname});
+});
+
+app.listen(port, hostname);
---
+++ public/scripts/timeline.js
@@ -0,0 +1,34 @@
+const ul = document.getElementById('timepoint');
+
+const createNode = (element) => document.createElement(element); // Create the type of element you pass in the parameters
+const append = (parent, el) => parent.appendChild(el); //Append the element to the parent
+
+fetch("https://api.airtable.com/v0/appGU1IvnOZqFFiCh/Table%201?view=Grid%20view", {
+    headers: { Authorization: "Bearer "+ "keyvwuNlf64WbA2YK" } //fix authorization exposure
+  })
+  .then(data => data.json())
+  .then(data => {
+      data.records.map(record => {
+        let div = createNode('div'),
+            li = createNode('li'),
+            time = createNode('time'),
+            p1 = createNode('p'),
+            p2 = createNode('p');
+
+        //Setting date element and datelement attribute
+        time.innerHTML = record.fields['Publicado'];
+        const att = document.createAttribute("datetime");
+        att.value = record.fields['Publicado'];
+        time.setAttributeNode(att);
+
+        p1.innerHTML = record.fields['Breve descricao'];
+        p1.id = "timeline-text";
+
+        p2.innerHTML = record.fields['Canal'];
+        append(div, time);
+        append(div, p1);
+        append(div, p2);
+        append(li, div);
+        append(ul, li);
+  })})
+  .catch(err => console.log(err));
